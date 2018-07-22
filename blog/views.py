f2a7518512135bbd08ab090e40643fce59a2454b
xiao <383697894@qq.com>
2018-07-22 11:23:33 +0800

    添加分页

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,16 @@ import os
 from bs4 import BeautifulSoup
 from blog.form import UserForm
 from utils.code import check_code
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+# def page_html(request,data):
+#     try:
+#         page_num = int(request.GET.get('page', 1))
+#     except Exception:
+#         page_num = 1
+#
+#     return Pagination(page_num, len(data))
 
 def code(request):
     """
@@ -78,7 +88,31 @@ def required_login(func):
 
 def index(request):  # 首页
     article_list = Article.objects.all()  # 查询所有文章
-    return render(request, "index.html", {"article_list": article_list})
+    paginator = Paginator(article_list, 3)  # 每页显示2条
+
+    # 异常判断
+    try:
+        # 当前页码，如果取不到page参数，默认为1
+        current_num = int(request.GET.get("page", 1))  # 当前页码
+        article_list = paginator.page(current_num)  # 获取当前页码的数据
+    except Exception:  # 出现异常时
+        article_list = paginator.page(1)  # 强制更新为第一页
+        current_num = 1
+
+    #  如果页数十分多时，换另外一种显示方式
+    if paginator.num_pages > 9:  # 一般网页展示11页,左5页,右5页,加上当前页,共11页
+        if current_num - 4 < 1:  # 如果前5页小于1时
+            pageRange = range(1, 9)  # 页码的列表:范围是初始状态
+        elif current_num + 4 > paginator.num_pages:  # 如果后5页大于总页数时
+            # 页码的列表:范围是(当前页-5,总页数+1)。因为range顾头不顾尾,需要加1
+            pageRange = range(current_num - 4, paginator.num_pages + 1)
+        else:
+            # 页码的列表:后5页正常时,页码范围是(当前页-5,当前页+6)。注意不是+5,因为range顾头不顾尾！
+            pageRange = range(current_num - 4, current_num + 5)
+    else:
+        pageRange = paginator.page_range  # 页码的列表
+
+    return render(request, "index.html", {"article_list": article_list,"paginator": paginator, "current_num": current_num,"pageRange":pageRange})
 
 def logout(request):  # 注销
     auth.logout(request)
